98f8af354b62b70c38846fa71745d0a321d72c22
Tomas <tmercadoslp@gmail.com>
2024-11-22 12:05:09 -0300

    feat: add calculator into the endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
+import bodyParser from "body-parser";
 import express from "express";
+import z from "zod";
+import { calculate } from "./calculate";
 
 const DEFAULT_PORT = 3000;
 const PORT = process.env.PORT || DEFAULT_PORT;
 
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+
+const calculateBodySchema = z.object({
+  operation: z.string().min(1),
+});
+
+app.post("/calculate", (req, res) => {
+  const bodyParseResult = calculateBodySchema.safeParse(req.body);
+
+  if (!bodyParseResult.success) {
+    res.status(400).json({ error: bodyParseResult.error.message });
+    return;
+  }
+
+  const { operation } = bodyParseResult.data;
+
+  try {
+    const result = calculate(operation);
+    res.json({ result });
+  } catch (e) {
+    const error = e as Error;
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
